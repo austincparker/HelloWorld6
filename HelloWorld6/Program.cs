bef200b43a0a1e37dbb0e8366e89ea363ab5a361
austinparker <austinparker@AUSTINPARKE4D2B>
2022-01-11 19:14:28 -0600

    Add project files.

---
+++ HelloWorld6/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Howdy Y'all!!");
+Console.ReadKey();
+var animals = new string[] { "Triceratops", "Gorilla", "Corgi", "Toucan" };
+
+foreach (var animal in animals)
+{
+	if (animal.Length > 5)
+    {
+	Console.WriteLine(animal);
+    }
+    else
+    {
+        Console.WriteLine("too short");
+    }
+}
